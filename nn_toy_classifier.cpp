eacc682806ae7528bd0bc9c382b615f712d84a73
Wayne Cochran <waynec@quintar.ai>
2023-01-07 13:39:05 -0800

    Initial commit. Some quick design decisions.

---
+++ nn_toy_classifier.cpp
@@ -0,0 +1,79 @@
+#include <vector>
+#include <memory>
+#include <cassert>
+#include <Eigen/Dense>
+
+class Module {
+    const size_t _numInputs, _numOutputs;
+public:
+    Module(size_t in, size_t out) : _numInputs{in}, _numOutputs{out} {}
+    virtual ~Module() {};
+    size_t numInputs() const {return _numInputs;}
+    size_t numOutputs() const {return _numOutputs;}
+    virtual const Eigen::VectorXf& operator()(const Eigen::VectorXf& input) = 0;
+    virtual const Eigen::VectorXf& output() const = 0;
+};
+
+class LinearModule : public Model { // fully connected
+    Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> _W;
+    Eigen::Vector<float, Eigen::Dynamic> _b;
+    Eigen::Vector<float, Eigen::Dynamic> _z;
+public:
+    LinearModel(size_t in, size_t out)
+        : Module(in, out), _W(out,in), _b(out), _z(out) {}
+    virtual const Eigen::VectorXf& operator()(const Eigen::VectorXf& x) {
+        assert(input.rows() == numInputs());
+        _z.noalias() = _W*x + _b;
+        return _z;
+    }
+    virtual const Eigen::VectorXf& output() const {return _z;}
+};
+
+class ReLUModule : public Module {
+    Eigen::Vector<float, Eigen::Dynamic> _a;
+public:
+    RelUModule(size_t inOut) : Module(inOut, InOut) _a(inOut) {}
+    virtual const Eigen::VectorXf& operator()(const Eigen::VectorXf& x) {
+        _a.noalias() = x.unaryExpr([](float elem) -> float {
+            return std::max(0.f,elem);
+        });
+        return _a;
+    }
+    virtual const Eigen::VectorXf& output() const {return _a;}
+};
+
+class SoftMaxModule : public Module {
+    Eigen::Vector<float, Eigen::Dynamic> _a;
+public:
+    SoftMaxModule(size_t inOut) : Module(inOut, InOut) _a(inOut) {}
+    virtual const Eigen::VectorXf& operator()(const Eigen::VectorXf& x) {
+        assert(c.rows == numInputs());
+        const Eigen::VectorXf e2x = x.unaryExpr([](float elem) -> float {
+            return std::exp(elem);
+        });
+        const float sum = e2x.sum();
+        _a.noalias() = e2x.unaryExpr([=sum](float elem) -> float {
+            return elem/sum;
+        });
+        return _a;
+    }
+    virtual const Eigen::VectorXf& output() const {return _a;}
+};
+
+class SequentialModel : public Module {
+    std::vector<std::unique_ptr<Module>> _modules;
+public:
+    SequentialModule(std::vector<std::unique_ptr<Module>>&& modules)
+        : Module(modules.first().numInputs(), modules.last().numOutputs()),
+          _modules{std::move(modules) {
+        y = Eigen::VectorXf(_modules.last().numOutputs());
+    }
+    virtual const Eigen::VectorXf& operator()(const Eigen::VectorXf& x) {
+        _modules[0](x);
+        for (size_t i = 1; i < _modules.size(); i++) {
+            _modules[i](_modules[i-1].output());
+        }
+        return modules.last().output();
+    }
+    virtual const Eigen::VectorXf& output() const {return modules.last().output();}
+};
